d6d31a0976ac4238f952c5c68c69e762605364b8
sarahtbnu <sarah.tebonou@googlemail.com>
2026-06-13 10:07:53 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const slides = document.querySelectorAll(".slide");
 let currentSlide = 0;
 
-function nextSlide() {
+function nextSlide(withSparkle = false) {
   slides[currentSlide].classList.remove("active");
   currentSlide++;
 
@@ -10,10 +10,34 @@ function nextSlide() {
   }
 
   slides[currentSlide].classList.add("active");
+
+  if (withSparkle) {
+    sparkle();
+  }
 }
 
 function restart() {
   slides[currentSlide].classList.remove("active");
   currentSlide = 0;
   slides[currentSlide].classList.add("active");
+  sparkle();
+}
+
+function sparkle() {
+  for (let i = 0; i < 26; i++) {
+    const star = document.createElement("div");
+
+    star.textContent = Math.random() > 0.5 ? "✨" : "🤍";
+    star.classList.add("sparkle");
+
+    star.style.left = Math.random() * 100 + "vw";
+    star.style.top = Math.random() * 100 + "vh";
+    star.style.animationDuration = Math.random() * 1.5 + 1.5 + "s";
+
+    document.body.appendChild(star);
+
+    setTimeout(function () {
+      star.remove();
+    }, 3000);
+  }
 }
